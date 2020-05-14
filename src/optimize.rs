2e8fbdf82ad71f7dcdedf5ef52f6e706c6cc12e8
dingo-actual <ryan@beta-reduce.net>
2020-05-14 11:48:23 -0500

    refactoring

--- src/optimize.rs
+++ src/optimize.rs
@@ -1,12 +1,6 @@
-use crate::generics::operators::crossover::Crossover;
-use crate::generics::operators::local_search::LocalSearch;
-use crate::generics::operators::mutation::Mutation;
-use crate::generics::operators::select_parents::SelectParents;
-use crate::generics::operators::select_survivors::SelectSurvivors;
-use crate::generics::population::population::Population;
-use crate::generics::population::solution::Solution;
+use crate::generics::operators::*;
+use crate::generics::population::*;
 use crate::generics::terminator::Terminator;
-use crate::util::uid::UidStack;
 
 fn optimize<G: Clone + Copy, P: Clone + Copy>(pop: &Population<G,P>, fitness: &Fitness<G,P>, diversity: &Diversity<G,P>, xover: &Crossover<G,P>, mutate: &Mutation<G,P>, local_search: &LocalSearch<G,P>, select_parents: SelectParents<G,P>, select_survivors: SelectSurvivors<G,P>, term: Terminator<G,P>) -> () {
     let mut terminate = False;
